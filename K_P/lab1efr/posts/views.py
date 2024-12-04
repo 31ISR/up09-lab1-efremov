e3a63f07f16af3f4afbac8ea20c4fd42b39f4a72
ST3412 <ST3412@TE.local>
2024-12-04 17:24:41 +0600

    {laba2}

--- K_P/lab1efr/posts/views.py
+++ K_P/lab1efr/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Post
 def posts_list(req):
-    return render(req, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(req, 'posts/posts_list.html', {'posts': posts}) 
 
 # Create your views here.
